110cd8b0ab8d90e3235ff30cb9f7b6829128065a
Beatriz Francelino <beatriz.fbcarneiro@gmail.com>
2022-02-21 12:52:47 -0300

    fixação do switch

---
+++ Módulo JavaScript/switch/exercícios.js
@@ -0,0 +1,24 @@
+//Crie uma variável chamada “fruta”. 
+//Esta variável deve receber uma string com o nome de uma fruta.
+// Após, crie uma estrutura condicional switch que receba esta variável e que possua três casos: 
+//caso maçã, retorne no console: “Não vendemos esta fruta aqui”. Caso kiwi, retorne: “Estamos com escassez de kiwis” 
+//e caso melancia, retorne: “Aqui está, são 3 reais o quilo”.
+// Teste com estas três opções e verifique o console do seu navegador.
+// Crie também um default, que retornará uma mensagem de erro no console.
+
+let fruta =  prompt("Digite o nome de uma fruta") 
+switch(fruta){
+    case "maça":
+    alert("Não vendemos esta fruta aqui")
+break
+case "kiwi":
+alert("Estamos com escassez de kiwis")
+break
+
+case "melancia":
+alert("Aqui está, são 3 reais o quilo")
+break;
+}
+
+
+// yaaaaaaaaaaaaaay consegui sozinha 
---
+++ Módulo JavaScript/switch/exercícios2.js
@@ -0,0 +1,15 @@
+//Um homem decidiu ir à uma revenda comprar um carro. 
+//Ele deseja comprar um carro hatch, e a revenda possui, além de carros hatch, sedans,
+// motocicletas e caminhonetes. Utilizando uma estrutura switch/case, caso o comprador queira o hatch,
+// retorne: “Compra efetuada com sucesso”. Nas outras opções, retorne: “Tem certeza que não prefere este modelo?”.
+// Caso seja especificado um modelo que não está disponível,
+// retorne no console: “Não trabalhamos com este tipo de automóvel aqui”.
+
+let modelo = prompt("Olá, deseja ver qual modelo: \nHatch\nSedans \nMotocicletas \nCaminhonetes")
+switch(modelo){
+    case"Hatch":
+    alert("Compra efetuada com sucesso")
+    break;
+    default: 
+alert ("Tem certeza que não prefere este modelo?")
+}
