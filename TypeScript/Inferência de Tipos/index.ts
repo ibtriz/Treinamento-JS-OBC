7d09adc4910259a665efc7b91845048e71611bb4
Beatriz Francelino <beatriz.fbcarneiro@gmail.com>
2022-03-24 15:34:14 -0300

    typescript

---
+++ TypeScript/Inferência de Tipos/index.ts
@@ -0,0 +1,10 @@
+  const spaceship = {
+    name: 'X-Wing',
+    speed: 0
+  };
+  
+  function accelerate(spaceship: { name: string; speed: number; }, speed: number) {
+    
+  }
+  
+  accelerate(spaceship, 50);
