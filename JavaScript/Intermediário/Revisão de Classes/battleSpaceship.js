fdc9b041006260c24190b422d734b5ab4b48920d
Beatriz Francelino <beatriz.fbcarneiro@gmail.com>
2022-04-28 13:10:21 -0300

    exercícios

--- JavaScript/Intermediário/Revisão de Classes/battleSpaceship.js
+++ JavaScript/Intermediário/Revisão de Classes/battleSpaceship.js
@@ -1,6 +1,6 @@
 class BattleSpaceship extends Spaceship {
       constructor(name, crewQuantity, weaponsQuantity) {
-        super(name, crewQuantity)
+        super(name, crewQuantity) // dizendo para herdar esses mesmos atributos da classe mãe
         this.weaponsQuantity = weaponsQuantity
       }
     }
