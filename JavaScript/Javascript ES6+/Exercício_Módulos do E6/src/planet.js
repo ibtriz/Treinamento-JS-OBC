64cb560bfb1fe483a79f332f97377af80d37be38
Beatriz Francelino <beatriz.fbcarneiro@gmail.com>
2022-05-10 11:18:28 -0300

    exercício sobre módulo Common.js

---
+++ JavaScript/Javascript ES6+/Exercício_Módulos do E6/src/planet.js
@@ -0,0 +1,7 @@
+export default class Planet {
+    constructor(name, auToSun) {
+      this.name = name
+      this.auToSum = auToSun
+    }
+  }
+  
---
+++ JavaScript/Javascript ES6+/Exercício_Módulos do E6/src/planet_operations.js
@@ -0,0 +1,4 @@
+export const AU_IN_KILOMETERS = 149587870
+export function convertAUtoKm(au) {
+    return au * AU_IN_KILOMETERS
+}
