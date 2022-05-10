8b29d7c7a7298c42d72e4b63549e5f1384395f41
Beatriz Francelino <beatriz.fbcarneiro@gmail.com>
2022-05-10 10:26:52 -0300

    Introdução a webpack

---
+++ JavaScript/Javascript ES6+/Utilizando Webpack/src/index.js
@@ -0,0 +1,2 @@
+import moment from 'moment/src/moment'
+alert(moment().format("hh:mm:ss") )
---
+++ JavaScript/Javascript ES6+/Utilizando Webpack/src/webpack.conf.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //vai gerar uma entrada que vai chamar de index
+  entry: {
+    index: "./src/index.js", //quando ler o arquivo src/index.js
+    //quando não tem essa especificação, o padrão é procura o index.js e gerar o main
+  },
+};
