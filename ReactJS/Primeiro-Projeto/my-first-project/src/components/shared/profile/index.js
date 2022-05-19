9a525c75deb363e3da96ddf49af61f49a1db1af9
Beatriz Francelino <beatriz.fbcarneiro@gmail.com>
2022-05-19 10:31:03 -0300

    style: centering of images

--- ReactJS/Primeiro-Projeto/my-first-project/src/components/shared/profile/index.js
+++ ReactJS/Primeiro-Projeto/my-first-project/src/components/shared/profile/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import './style.css'
 
 function Profile(props) {
   return (
     <div id="Profile">
-      <h2 className="villainName">{props.villainName}</h2>
-      <p className="description">{props.description}</p>
-      <img className="Imagem" src={props.img_url} alt={props.personagem} />
+      <h2 class="villainName">{props.villainName}</h2>
+      <p class="description">{props.description}</p>
+      <img class="Imagem" src={props.img_url} alt={props.personagem} />
       <br />
       <a href="{props.link}">{props.saibamais}</a>
     </div>
